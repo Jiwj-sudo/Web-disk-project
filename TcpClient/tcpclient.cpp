b89c1afb11b97178adffa2e03024234d319c5d80
Jiwj-sudo <jweijun0218@yeah.net>
2023-09-26 14:44:58 +0800

    第一次提交

---
+++ TcpClient/tcpclient.cpp
@@ -0,0 +1,60 @@
+﻿#include "tcpclient.h"
+#include "ui_tcpclient.h"
+#include <QByteArray>
+#include <QDebug>
+#include <QMessageBox>
+#include <QHostAddress>
+
+TcpClient::TcpClient(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::TcpClient)
+{
+    ui->setupUi(this);
+    // 加载配置
+    loadConfig();
+    // 连接服务器
+    connectionServer();
+}
+
+TcpClient::~TcpClient()
+{
+    delete ui;
+}
+/*
+ 加载配置文件，配置文件中存有ip地址和端口号
+*/
+void TcpClient::loadConfig()
+{
+    QFile file(":/client.config");
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        QMessageBox::critical(this, "open config", "open config failed");
+    }
+
+    QByteArray baData = file.readAll();
+    QString strData = baData.toStdString().c_str();
+    // qDebug() << strData;
+    file.close();
+
+    strData.replace("\r\n", " ");
+
+    QStringList strList = strData.split(" ");
+    m_strIP = strList[0];
+    m_usPort = strList[1].toUShort();
+}
+
+void TcpClient::connectionServer()
+{
+    //connect(&m_tcpSocket, SIGNAL(connectd()), this, SLOT(showConnect()));
+    connect(&m_tcpSocket, &QTcpSocket::connected, this, &TcpClient::showConnect);
+
+
+    // 连接服务器
+    m_tcpSocket.connectToHost(QHostAddress(m_strIP), m_usPort);
+}
+
+void TcpClient::showConnect()
+{
+    QMessageBox::information(this, "连接服务器", "连接服务器成功");
+}
+
---
+++ TcpClient/tcpclient.h
@@ -0,0 +1,32 @@
+﻿#ifndef TCPCLIENT_H
+#define TCPCLIENT_H
+
+#include <QWidget>
+#include <QFile>
+#include <QTcpSocket>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class TcpClient; }
+QT_END_NAMESPACE
+
+class TcpClient : public QWidget
+{
+    Q_OBJECT
+
+public:
+    TcpClient(QWidget *parent = nullptr);
+    ~TcpClient();
+    void loadConfig();
+    void connectionServer();
+
+public slots:
+    void showConnect();
+
+private:
+    Ui::TcpClient *ui;
+    QString m_strIP;
+    quint16 m_usPort;
+    //连接服务器,和服务器进行数据交互
+    QTcpSocket m_tcpSocket;
+};
+#endif // TCPCLIENT_H
