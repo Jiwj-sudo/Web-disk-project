cf4561d8a5f7e324e98a3c6ffdaabf70a7db2ebc
Jiwj-sudo <jweijun0218@yeah.net>
2023-09-27 17:43:49 +0800

    提交用户注册

--- TcpClient/tcpclient.cpp
+++ TcpClient/tcpclient.cpp
@@ -9,7 +9,8 @@ TcpClient::TcpClient(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::TcpClient)
 {
-    this->setFixedSize(380,250);
+    // this->resize(100, 250);
+    // this->setFixedSize(380,250);
     ui->setupUi(this);
     // 加载配置
     loadConfig();
@@ -48,6 +49,7 @@ void TcpClient::connectionServer()
 {
     //connect(&m_tcpSocket, SIGNAL(connectd()), this, SLOT(showConnect()));
     connect(&m_tcpSocket, &QTcpSocket::connected, this, &TcpClient::showConnect);
+    connect(&m_tcpSocket, &QTcpSocket::readyRead, this, &TcpClient::recvMsg);
 
     // 连接服务器
     m_tcpSocket.connectToHost(QHostAddress(m_strIP), m_usPort);
@@ -58,7 +60,38 @@ void TcpClient::showConnect()
     QMessageBox::information(this, "连接服务器", "连接服务器成功");
 }
 
+void TcpClient::recvMsg()
+{
+    qDebug() << m_tcpSocket.bytesAvailable();
+    uint uiPDULen = 0;
+    m_tcpSocket.read((char*)&uiPDULen, sizeof(uint));
+    uint uiMsgLen = uiPDULen - sizeof(PDU);
 
+    PDU* pdu = mkPDU(uiMsgLen);
+    m_tcpSocket.read((char*)pdu+sizeof(uint), uiPDULen - sizeof(uint));
+    switch(pdu->uiMsgType)
+    {
+    case ENUM_MSG_TYPE_REGIST_RESPOND:
+    {
+        if(0 == strcmp(pdu->caData, REGIST_OK))
+        {
+            QMessageBox::information(this, "注册", REGIST_OK);
+        }
+        else if (0 == strcmp(pdu->caData, REGIST_FAILED))
+        {
+            QMessageBox::warning(this, "注册", REGIST_FAILED);
+        }
+        break;
+    }
+    default:
+        break;
+    }
+
+    free(pdu);
+    pdu = nullptr;
+}
+
+#if 0
 void TcpClient::on_send_pb_clicked()
 {
     QString strMsg = ui->lineEdit->text();
@@ -75,5 +108,39 @@ void TcpClient::on_send_pb_clicked()
     {
         QMessageBox::warning(this, "信息发送", "发送的信息不能为空");
     }
+}
+#endif
+
+
+void TcpClient::on_login_pb_clicked()
+{
+
+}
+
+
+void TcpClient::on_regist_pb_clicked()
+{
+    QString strName = ui->name_le->text();
+    QString strPwd = ui->pwd_le->text();
+    if (!strName.isEmpty() && !strPwd.isEmpty())
+    {
+        PDU* pdu = mkPDU(0);
+        pdu->uiMsgType = ENUM_MSG_TYPE_REGIST_REQUEST;
+        strncpy(pdu->caData, strName.toStdString().c_str(), 32);
+        strncpy(pdu->caData+32, strPwd.toStdString().c_str(), 32);
+        m_tcpSocket.write((char*)pdu, pdu->uiPDULen);
+        free(pdu);
+        pdu = nullptr;
+    }
+    else
+    {
+        QMessageBox::critical(this, "注册", "注册失败,用户名或密码为空!");
+    }
+}
+
+
+void TcpClient::on_cancel_pb_clicked()
+{
+
 }
 
--- TcpClient/tcpclient.h
+++ TcpClient/tcpclient.h
@@ -22,9 +22,16 @@ public:
 
 public slots:
     void showConnect();
+    void recvMsg();
 
 private slots:
-    void on_send_pb_clicked();
+    // void on_send_pb_clicked();
+
+    void on_login_pb_clicked();
+
+    void on_regist_pb_clicked();
+
+    void on_cancel_pb_clicked();
 
 private:
     Ui::TcpClient *ui;
